bfbe279e78adad69633ffee34fd57167c51f0b33
Leandro Vilas Boas <leandrovboas@msn.com>
2018-03-01 15:46:12 -0300

    Incluindo serviço de tradução de texto

---
+++ Maratona.Bots.Microsoft.Books/SevicoLinguagem.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Maratona.Bots.Microsoft.Books
+{
+    public class SevicoLinguagem
+    {
+        private readonly string _translateApiKey = ConfigurationManager.AppSettings["TranslateApiKey"];
+        private readonly string _translateUri = ConfigurationManager.AppSettings["TranslateUri"];
+
+        public async Task<string> TraducaoDeTextoAsync(string texto)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _translateApiKey);
+
+                var uri = _translateUri + "?to=pt-br" +
+                          "&text=" + System.Net.WebUtility.UrlEncode(texto);
+
+                var response = await client.GetAsync(uri);
+                var result = await response.Content.ReadAsStringAsync();
+                var content = XElement.Parse(result).Value;
+
+                return $"Texto original: **{ texto }**\nTradução: **{ content }**";
+            }
+        }
+    }
+}
